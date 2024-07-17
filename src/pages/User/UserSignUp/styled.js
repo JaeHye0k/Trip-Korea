f3d0b7a991adb4475aae67bff1ac47f38e330cdf
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-07-17 19:33:53 +0900

    Error 스타일드 컴포넌트 추가

--- src/pages/User/UserSignUp/styled.js
+++ src/pages/User/UserSignUp/styled.js
@@ -1,47 +1,53 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
-  width: 100%;
-  max-width: 500px;
-  margin: 0 auto;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  span {
-    display: block;
-    text-align: center;
-    margin-top: 20px;
-    a {
-      color: #ff9900;
-    }
-  }
+	width: 100%;
+	max-width: 500px;
+	margin: 0 auto;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	span {
+		display: block;
+		text-align: center;
+		margin-top: 20px;
+		a {
+			color: #ff9900;
+		}
+	}
 `;
 
 export const Logo = styled.div`
-  width: 100%;
-  max-width: 200px;
-  margin: 0 auto 100px;
-  cursor: pointer;
-  &:hover {
-    opacity: 0.5;
-  }
+	width: 100%;
+	max-width: 200px;
+	margin: 0 auto 100px;
+	cursor: pointer;
+	&:hover {
+		opacity: 0.5;
+	}
 `;
 
 export const Form = styled.form`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  input {
-    border: 1px solid #ccc;
-    padding: 15px;
-    border-radius: 10px;
-  }
+	margin-top: 20px;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	input {
+		border: 1px solid #ccc;
+		padding: 15px;
+		border-radius: 10px;
+	}
 
-  input[type="submit"] {
-    background: #000;
-    color: #fff;
-    cursor: pointer;
-  }
+	input[type="submit"] {
+		background: #000;
+		color: #fff;
+		cursor: pointer;
+	}
+`;
+
+export const Error = styled.p`
+	color: red;
+	margin-top: 1rem;
+	text-align: center;
 `;
